241b435479f5f4dc638b017aec3a22510ec78b40
Victor Lima <joaozinhobaodebriga@gmail.com>
2025-11-24 22:36:33 -0300

    Add files via upload
    backend finalizado

--- SemSaldo/src/main/java/com/example/SemSaldo/model/Product.java
+++ SemSaldo/src/main/java/com/example/SemSaldo/model/Product.java
@@ -3,65 +3,78 @@ package com.example.SemSaldo.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "product")
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String Nome;
-	private String Descricao;
-	private Integer Gtin;
-	private Integer Qtd;
-	private String Endereco;
+	private String nome;
+	private String descricao;
+	private Integer gtin;
+	private Integer qtd;
+	private String endereco;
+
+	
+	public Product(){}
 	
 	public Product(Long id , String nome, String descricao, Integer gtin, Integer qtd, String endereco) {
 		
 		this.id = id;
-		this.setName(nome);
-		this.setDescricao(descricao);
-		this.setGtin(gtin);
-		this.setQtd(qtd);
-		this.setEndereco(endereco);
+		this.nome = nome;
+		this.descricao = descricao;
+		this.gtin = gtin;
+		this.qtd = qtd;
+		this.endereco = endereco;
+		
+		
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+	public String getNome() {
+		return nome;
 	}
 
-	public String getName() {
-		return Nome;
-	}
-
-	public void setName(String nome) {
-		Nome = nome;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getDescricao() {
-		return Descricao;
+		return descricao;
 	}
 
 	public void setDescricao(String descricao) {
-		Descricao = descricao;
+		this.descricao = descricao;
 	}
 
 	public Integer getGtin() {
-		return Gtin;
+		return gtin;
 	}
 
 	public void setGtin(Integer gtin) {
-		Gtin = gtin;
+		this.gtin = gtin;
 	}
-
+	
 	public Integer getQtd() {
-		return Qtd;
+		return qtd;
 	}
 
 	public void setQtd(Integer qtd) {
-		Qtd = qtd;
+		this.qtd = qtd;
 	}
 
 	public String getEndereco() {
-		return Endereco;
+		return endereco;
 	}
 
 	public void setEndereco(String endereco) {
-		Endereco = endereco;
+		this.endereco = endereco;
 	}
-
-
+	
+	
 }
