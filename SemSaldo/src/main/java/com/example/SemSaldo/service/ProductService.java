893b25b134c1c07859e3210ae9d32d874a04df97
Victor Lima <joaozinhobaodebriga@gmail.com>
2025-11-27 21:36:48 -0300

    Add files via upload
    Adicionado um botão para baixar uma planilha e excel, adição de mais uma variável no forms e uma data de registro quando injetado alguma operação

--- SemSaldo/src/main/java/com/example/SemSaldo/service/ProductService.java
+++ SemSaldo/src/main/java/com/example/SemSaldo/service/ProductService.java
@@ -1,6 +1,11 @@
 package com.example.SemSaldo.service;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import com.example.SemSaldo.model.Product;
 import com.example.SemSaldo.repository.ProductRepository;
@@ -15,5 +20,37 @@ public class ProductService {
 	}
 	public List<Product> getAll(){return repository.findAll();}
 	public Product save(Product product) {return repository.save(product);}
+	
+	public byte[] gerarExcelProdutos() throws Exception{
+		List<Product> produtos = repository.findAll();
+		
+		Workbook workbook = new XSSFWorkbook();
+		org.apache.poi.ss.usermodel.Sheet sheet = workbook.createSheet("Produtos");
+		
+		Row header = sheet.createRow(0);
+		header.createCell(0).setCellValue("Nome");
+		header.createCell(1).setCellValue("Descrição");
+		header.createCell(2).setCellValue("Gtin");
+		header.createCell(3).setCellValue("Quantidade em Cx");
+		header.createCell(4).setCellValue("Endereço");
+		header.createCell(5).setCellValue("Situação do produto");
+		header.createCell(6).setCellValue("Data de Registro");
+		
+		int i = 1;
+		for (Product p: produtos) {
+			Row row = sheet.createRow(i++);
+			row.createCell(0).setCellValue(p.getNome());
+			row.createCell(1).setCellValue(p.getDescricao());
+			row.createCell(2).setCellValue(p.getGtin());
+			row.createCell(3).setCellValue(p.getQtd());
+			row.createCell(4).setCellValue(p.getEndereco());
+			row.createCell(5).setCellValue(p.getSituacao());
+			row.createCell(6).setCellValue(p.getDataRegistro());
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		workbook.write(out);
+		workbook.close();
+		return out.toByteArray();
+	}
 
 }
--- projeto_trabalho/src/components/Home.vue
+++ projeto_trabalho/src/components/Home.vue
@@ -7,12 +7,22 @@
             <p>Gtin do produto: <input type="number" required v-model="gtin"></p>
             <p>Quantidade de caixas do produto: <input type="number" required v-model="qtd"></p>
             <p>Endereco do produto: <input type="text" required v-model="endereco"></p>
+            <label>Situação do produto: </label>
+                <select v-model="situacao">
+                    <option value="sem saldo">Sem saldo</option>
+                    <option value="falta">Falta</option>
+                </select>
+                <br>
+                <br>
         <button type="submit">Adicionar produto</button>
+        <br>
+        <br>
+        <button @click="baixarExcel">Baixar em Excel</button>
         </form>
         <div>
             <p>Produtos sem saldo</p>
             <ul>
-                <li v-for="product in SemSaldo" :key="product.id">{{product.nome}},{{product.descricao}},{{product.gtin}},{{product.qtd}},{{product.endereco}}</li>
+                <li v-for="product in SemSaldo" :key="product.id">{{product.nome}}, {{product.descricao}}, {{product.gtin}}, {{product.qtd}}, {{product.endereco}}, {{ product.situacao }}</li>
             </ul>
         </div>
     </div>
@@ -27,6 +37,7 @@
                     gtin: "",
                     qtd: "",
                     endereco: "",
+                    situacao: "",
                     SemSaldo: [] 
                 }
             },
@@ -35,6 +46,20 @@
                 this.loadProduct()
             },
             methods: {
+                async baixarExcel(){
+                    const response = await fetch("http://localhost:8080/api/semsaldo/excel",{
+                    method: "GET"
+                    });
+                    const blob = await response.blob();
+                    const url = window.URL.createObjectURL(blob);
+
+                    const a = document.createElement("a");
+                    a.href = url;
+                    a.download = "auditoria_dos_produtos.xlsx";
+                    document.body.appendChild(a);
+                    a.click();
+                    a.remove();
+                },
                 async loadProduct(){
                     const response = await fetch("http://localhost:8080/api/semsaldo");
                     this.SemSaldo = await response.json();
@@ -45,7 +70,9 @@
                         descricao: this.descricao,
                         gtin: this.gtin,
                         qtd: this.qtd,
-                        endereco: this.endereco
+                        endereco: this.endereco,
+                        situacao: this.situacao
+                        
                     }
 
                     await fetch("http://localhost:8080/api/semsaldo",{
@@ -58,6 +85,7 @@
                     this.gtin = ""
                     this.qtd = ""
                     this.endereco = ""
+                    this.situacao = ""
 
                     this.loadProduct()
                 }
